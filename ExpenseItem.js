7be26b9697da743964539615bce625e821162a2d
DivyaJukuntla1999 <126849699+DivyaJukuntla1999@users.noreply.github.com>
2023-08-03 15:28:08 +0530

    ExpenseItem.css

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,11 +1,13 @@
+import './ExpenseItem.css';
 function ExpenseItem() {
     return (
-        <><div>
-        </div><h2>Food</h2><div>Rs 10</div><div>
+        <><div className="expense-item"> 
+        </div><h2>Food</h2><div>Rs 50</div><div>
             <div><h2>Petrol</h2></div>Rs 100<div></div>
             <div><h2>Movies</h2></div>Rs 200<div></div>
+            <div><h2>LocationOfExpenditure</h2><div>Dharwad</div></div>
             </div></>
-    )
+    );
 
 }
 export default ExpenseItem;
