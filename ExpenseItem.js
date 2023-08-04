9c536ff3031ab0b4537a138b121aced9186f3223
DivyaJukuntla1999 <126849699+DivyaJukuntla1999@users.noreply.github.com>
2023-08-04 15:58:01 +0530

    passing data

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,13 +1,25 @@
-import './ExpenseItem.css';
-function ExpenseItem() {
-    return (
-        <><div className="expense-item"> 
-        </div><h2>Food</h2><div>Rs 50</div><div>
-            <div><h2>Petrol</h2></div>Rs 100<div></div>
-            <div><h2>Movies</h2></div>Rs 200<div></div>
-            <div><h2>LocationOfExpenditure</h2><div>Dharwad</div></div>
-            </div></>
-    );
+import React from "react"; // Don't forget to import React
 
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) { // Add curly braces for function definition
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 500;
+  const locationOfExpenditure = 'Hubli';
+
+  return ( 
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div>{props.location} - {props.date.toISOString()}</div> {/* Assuming props.location and props.date are passed from the parent component */}
+      <div className="expense-item__description">
+        <h2>{expenseTitle} - {locationOfExpenditure}</h2>
+        <div className="expense-item__price">Rs. {expenseAmount}</div>
+        <h2>{props.title}</h2> {/* Assuming props.title and props.amount are passed from the parent component */}
+        <div className="expense-item__price">Rs. {props.amount}</div>
+      </div>
+    </div>
+  );
 }
-export default ExpenseItem;
+
+export default ExpenseItem; // Don't forget to export the component
