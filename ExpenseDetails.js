e1f253ea504c624e34143e88a0213a6fdd8a86ed
DivyaJukuntla1999 <126849699+DivyaJukuntla1999@users.noreply.github.com>
2023-08-17 00:27:04 +0530

    Basics of react.js

---
+++ ExpenseDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ExpenseDetails(props) {
+  return (
+    <div className="expense-details">
+      <h2>{props.title}</h2>
+      <div className="expense-details__price"> Rs. {props.amount}</div>
+      <div className="expense-details__location"> {props.location}</div>
+    </div>
+  );
+}
+
+export default ExpenseDetails;
--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,25 +1,19 @@
-import React from "react"; // Don't forget to import React
+import React from 'react';
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails'; // Import the new ExpenseDetails component
+import './ExpenseItem.css';
 
-import "./ExpenseItem.css";
-
-function ExpenseItem(props) { // Add curly braces for function definition
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 500;
-  const locationOfExpenditure = 'Hubli';
-
-  return ( 
+function ExpenseItem(props) {
+  return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
-      <div>{props.location} - {props.date.toISOString()}</div> {/* Assuming props.location and props.date are passed from the parent component */}
-      <div className="expense-item__description">
-        <h2>{expenseTitle} - {locationOfExpenditure}</h2>
-        <div className="expense-item__price">Rs. {expenseAmount}</div>
-        <h2>{props.title}</h2> {/* Assuming props.title and props.amount are passed from the parent component */}
-        <div className="expense-item__price">Rs. {props.amount}</div>
-      </div>
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails
+        title={props.title}
+        amount={props.amount}
+        location={props.location}
+      />
     </div>
   );
 }
 
-export default ExpenseItem; // Don't forget to export the component
+export default ExpenseItem;
