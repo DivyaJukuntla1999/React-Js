c5fcd5c7d836e38626c9d4c24440f3266991cd3c
DivyaJukuntla1999 <126849699+DivyaJukuntla1999@users.noreply.github.com>
2023-08-23 14:20:04 +0530

    lifting up the data from child to parent
    Making the expense form input visible on screen.

--- NewExpense.js
+++ NewExpense.js
@@ -1,13 +1,21 @@
 import React from "react";
-
 import ExpenseForm from "./ExpenseForm";
 import './NewExpense.css';
-const NewExpense = () => {
-    return <div className="new-expense">
-        <ExpenseForm/>
-        
-    </div>
-};
 
+const NewExpense = (props) => { // Add 'props' as a parameter
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const enteredData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        props.onAddExpense(enteredData); // Fix the variable name here
+    };
+
+    return ( 
+        <div className="new-expense">
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+                </div>
+    );
+};
 
 export default NewExpense;
